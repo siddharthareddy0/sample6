5eee05d4d8974d3547690cd4cd6ed20a1554e472
siddhartha reddy <reddynarra143@gmail.com>
2024-12-11 11:06:33 +0530

    main

---
+++ one_to_hun.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class one_to_hun{
+    public static void main(String[] args) {
+        int count;
+          List<Integer> li=new ArrayList<>();
+          for(int i=2;i<=100;i++)
+          {
+            count=0;
+            for(int j=1;j<=i;j++)
+            {
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count<=2){
+                li.add(i);
+            }
+          }
+          for(int i=0;i<li.size();i++){
+            System.out.print(li.get(i)+" ");
+          }
+    }
+}
