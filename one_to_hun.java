6a1f97e718c0240553645cac2c16521ffbbc3806
siddhartha reddy <reddynarra143@gmail.com>
2024-12-11 11:09:17 +0530

    commit-2

--- one_to_hun.java
+++ one_to_hun.java
@@ -6,7 +6,7 @@ public class one_to_hun{
           for(int i=2;i<=100;i++)
           {
             count=0;
-            for(int j=1;j<=i;j++)
+            for(int j=1;i<=i;j++)
             {
                 if(i%j==0){
                     count++;
